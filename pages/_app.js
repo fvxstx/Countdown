9dd76dbcf1acf453d843d8f2b34a9b65c15da629
Fausto Torres <faut.btorres@gmail.com>
2022-05-07 02:12:59 +0100

    fixing again crossorigin problem

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossorigin={true.toString()}
-        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap"
           rel="stylesheet"
