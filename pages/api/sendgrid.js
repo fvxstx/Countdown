6d3cfd4d308e7a58b34a12a787af41af9e9a1dc6
Fausto Torres <faut.btorres@gmail.com>
2022-07-06 22:03:41 +0100

    now we go

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -1,6 +1,9 @@
 import sendgrid from "@sendgrid/mail";
 
-sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+const NEXT_PUBLIC_SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imphdnp4dHd0Z3poZXB1emNoeWRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTUyMzI2OTAsImV4cCI6MTk3MDgwODY5MH0.upiXI3HDWV4RRmAz4TxKLy09uR_y6diRi_QRci52n50";
+
+sendgrid.setApiKey(NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
 export default async (req, res) => {
   const body = JSON.parse(req.body);
