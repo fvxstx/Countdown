6ab03ef539f82ee6c3e130ae3d1db19e098026cb
Fausto Torres <faut.btorres@gmail.com>
2022-07-10 12:07:36 +0100

    fixing api sendgrid

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -2,7 +2,7 @@ import sendgrid from "@sendgrid/mail";
 
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
-export default async (req, res) => {
+export default async function (req, res) {
   const body = JSON.parse(req.body);
 
   if (body.lastEmail == true) {
@@ -13,7 +13,7 @@ export default async (req, res) => {
       templateId: "d-1bb313b8ef3046bebf19a19da13db514",
     };
 
-    sendgrid.send(data);
+    await sendgrid.send(data);
     return res.status(200).json({ status: "Ok" });
   } else if (body.lastEmail == false) {
     const data = {
@@ -23,7 +23,7 @@ export default async (req, res) => {
       templateId: "d-d6246a69f8ef4ff6889f1699e71d1cf7",
     };
 
-    sendgrid.send(data);
+    await sendgrid.send(data);
     return res.status(200).json({ status: "Ok" });
   }
-};
+}
